80f7acf317dbb8a7bdda29e594b8751fd9cf90e8
devvnul <bylica.marek2@gmail.com>
2025-07-30 18:31:39 +0200

    Improved "About us" page styling

--- wp-content/themes/bylica-kancelaria/src/template-parts/about/about.php
+++ wp-content/themes/bylica-kancelaria/src/template-parts/about/about.php
@@ -12,7 +12,11 @@
     <div class="about__select-wrapper">
         <div class="about__selection about__select-1 active">
             <div class="about__character-basic">
-                <img class="" src="<?php echo get_template_directory_uri(); ?>/assets/images/about/a1.png" alt="">
+                <div class="about__img-wrapper">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/about/a1.png" alt="">
+                </div>
+
+                <div class="about__img-blur"></div>
 
                 <div class="about__character-short">
                     <h2 class="h1 about__character-name">Tomasz Bylica</h2>
@@ -23,8 +27,9 @@
                     </p>
                 </div>
             </div>
+
             <div class="about__character-more">
-                <div class="about__character-section">
+                <div class="about__character-section mt-4">
                     <h2 class="h2 about__character-header">
                         Kilka słów o mnie:
                     </h2>
@@ -41,7 +46,7 @@
                 </div>
 
                 <div class="about__character-section">
-                    <h2 class="h2 about__character-header about__cv-bullet">
+                    <h2 class="h2 about__character-header about__character-header--points">
                         <i class="ph-thin ph-gavel"></i>
                         Specjalizacje
                     </h2>
@@ -54,52 +59,56 @@
                 </div>
 
                 <div class="about__character-section">
-                    <h2 class="h2 about__character-header about__cv-bullet">
+                    <h2 class="h2 about__character-header about__character-header--points">
                         <i class="ph-thin ph-graduation-cap"></i>
                         Wykształcenie
                     </h2>
                     <div class="about__character-education">
                         <p class="about__character-education-item">
-                            <span class="about__character-education-institution">Uniwersytet Jagielloński, Wydział Prawa
-                                i
-                                Administracji</span>
-                            <span class="about__character-education-degree">Magister Prawa</span>
+                            <span class="about__character-education-institution">
+                                Uniwersytet Jagielloński, Wydział Prawa i Administracji
+                            </span>
+                            <span class="about__character-education-degree">
+                                Magister Prawa
+                            </span>
                         </p>
                         <p class="about__character-education-item">
-                            <span class="about__character-education-institution">Aplikacja radcowska przy Okręgowej
-                                Izbie
-                                Radców Prawnych w Krakowie</span>
-                            <span class="about__character-education-degree">Radca Prawny</span>
+                            <span class="about__character-education-institution">
+                                Aplikacja radcowska przy Okręgowej Izbie Radców Prawnych w Krakowie
+                            </span>
+                            <span class="about__character-education-degree">
+                                Radca Prawny
+                            </span>
                         </p>
                     </div>
                 </div>
 
                 <div class="about__character-section">
-                    <h2 class="h2 about__character-header about__cv-bullet">
+                    <h2 class="h2 about__character-header about__character-header--points">
                         <i class="ph-thin ph-briefcase"></i>
                         Doświadczenie
                     </h2>
                     <div class="about__character-experience">
                         <p class="about__character-experience-item">
-                            <span class="about__character-experience-role">Radca Prawny</span>
                             <span class="about__character-experience-company">Kancelaria Radcy Prawnego Bylica</span>
+                            <span class="about__character-experience-role">Radca Prawny</span>
                             <span class="about__character-experience-duration">2015 - obecnie</span>
                         </p>
                         <p class="about__character-experience-item">
-                            <span class="about__character-experience-role">Prawnik</span>
                             <span class="about__character-experience-company">Kancelaria Prawna XYZ</span>
+                            <span class="about__character-experience-role">Prawnik</span>
                             <span class="about__character-experience-duration">2010 - 2015</span>
                         </p>
                         <p class="about__character-experience-item">
-                            <span class="about__character-experience-role">Asystent Prawny</span>
                             <span class="about__character-experience-company">Kancelaria Prawna ABC</span>
+                            <span class="about__character-experience-role">Asystent Prawny</span>
                             <span class="about__character-experience-duration">2008 - 2010</span>
                         </p>
                     </div>
                 </div>
 
                 <div class="about__character-section">
-                    <h2 class="h2 about__character-header">
+                    <h2 class="h2 about__character-header about__character-header--points">
                         <i class="ph-thin ph-envelope"></i>
                         Kontakt
                     </h2>
@@ -116,8 +125,122 @@
                 </div>
             </div>
         </div>
+
         <div class="about__selection about__select-2 inactive">
-            <img class="" src="<?php echo get_template_directory_uri(); ?>/assets/images/about/a2.png" alt="">
+            <div class="about__character-basic">
+                <div class="about__img-wrapper">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/about/a2.png" alt="">
+                </div>
+
+                <div class="about__img-blur"></div>
+
+                <div class="about__character-short">
+                    <h2 class="h1 about__character-name">Anna Bylica</h2>
+                    <p class="about__character-role">Radca Prawny</p>
+                    <p class="about__character-description">
+                        Pomagam rozwiązywać wszelkie problemy natury prawnej, w szczególności z zakresu prawa
+                        gospodarczego, cywilnego, oraz administracyjnego i podatkowego.
+                    </p>
+                </div>
+            </div>
+
+            <div class="about__character-more">
+                <div class="about__character-section mt-4">
+                    <h2 class="h2 about__character-header">
+                        Kilka słów o mnie:
+                    </h2>
+                    <p class="about__character-bio">
+                        Jestem radcą prawnym z ponad 10-letnim doświadczeniem w branży. Specjalizuję się w prawie
+                        cywilnym, gospodarczym oraz administracyjnym. Moją misją jest zapewnienie klientom kompleksowej
+                        obsługi prawnej i pomoc w rozwiązywaniu ich problemów.
+                        W swojej pracy kieruję się zasadami etyki zawodowej, a także dążę do ciągłego podnoszenia
+                        swoich kwalifikacji. Ukończyłem studia na Uniwersytecie Jagiellońskim, a następnie aplikację
+                        radcowską przy Okręgowej Izbie Radców Prawnych w Krakowie. Posiadam bogate
+                        doświadczenie w pracy z klientami indywidualnymi oraz przedsiębiorstwami, co pozwala mi
+                        skutecznie doradzać i reprezentować ich interesy w sprawach prawnych.
+                    </p>
+                </div>
+
+                <div class="about__character-section">
+                    <h2 class="h2 about__character-header about__character-header--points">
+                        <i class="ph-thin ph-gavel"></i>
+                        Specjalizacje
+                    </h2>
+                    <ul class="about__character-specializations">
+                        <li>Prawo cywilne</li>
+                        <li>Prawo gospodarcze</li>
+                        <li>Prawo administracyjne</li>
+                        <li>Prawo podatkowe</li>
+                    </ul>
+                </div>
+
+                <div class="about__character-section">
+                    <h2 class="h2 about__character-header about__character-header--points">
+                        <i class="ph-thin ph-graduation-cap"></i>
+                        Wykształcenie
+                    </h2>
+                    <div class="about__character-education">
+                        <p class="about__character-education-item">
+                            <span class="about__character-education-institution">
+                                Uniwersytet Jagielloński, Wydział Prawa i Administracji
+                            </span>
+                            <span class="about__character-education-degree">
+                                Magister Prawa
+                            </span>
+                        </p>
+                        <p class="about__character-education-item">
+                            <span class="about__character-education-institution">
+                                Aplikacja radcowska przy Okręgowej Izbie Radców Prawnych w Krakowie
+                            </span>
+                            <span class="about__character-education-degree">
+                                Radca Prawny
+                            </span>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="about__character-section">
+                    <h2 class="h2 about__character-header about__character-header--points">
+                        <i class="ph-thin ph-briefcase"></i>
+                        Doświadczenie
+                    </h2>
+                    <div class="about__character-experience">
+                        <p class="about__character-experience-item">
+                            <span class="about__character-experience-company">Kancelaria Radcy Prawnego Bylica</span>
+                            <span class="about__character-experience-role">Radca Prawny</span>
+                            <span class="about__character-experience-duration">2015 - obecnie</span>
+                        </p>
+                        <p class="about__character-experience-item">
+                            <span class="about__character-experience-company">Kancelaria Prawna XYZ</span>
+                            <span class="about__character-experience-role">Prawnik</span>
+                            <span class="about__character-experience-duration">2010 - 2015</span>
+                        </p>
+                        <p class="about__character-experience-item">
+                            <span class="about__character-experience-company">Kancelaria Prawna ABC</span>
+                            <span class="about__character-experience-role">Asystent Prawny</span>
+                            <span class="about__character-experience-duration">2008 - 2010</span>
+                        </p>
+                    </div>
+                </div>
+
+                <div class="about__character-section">
+                    <h2 class="h2 about__character-header about__character-header--points">
+                        <i class="ph-thin ph-envelope"></i>
+                        Kontakt
+                    </h2>
+                    <div class="about__character-contact">
+                        <p class="about__character-contact-item">
+                            <span class="about__character-contact-label">Email:</span>
+                            <span class="about__character-contact-value">kontakt@kancelaria-bylica.pl</span>
+                        </p>
+                        <p class="about__character-contact-item">
+                            <span class="about__character-contact-label">Telefon:</span>
+                            <span class="about__character-contact-value">+48 123 456 789</span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </div>
 
