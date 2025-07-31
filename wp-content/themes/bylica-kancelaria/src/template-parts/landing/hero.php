dd253ce0c727a23e6fe323a1ec5b72fb2bd6775c
devvnul <bylica.marek2@gmail.com>
2025-07-31 17:23:59 +0200

    Hero section but its a mess

--- wp-content/themes/bylica-kancelaria/src/template-parts/landing/hero.php
+++ wp-content/themes/bylica-kancelaria/src/template-parts/landing/hero.php
@@ -1,7 +1,21 @@
 <div class="hero"
-    style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/landing/hero-bg.png')">
+    style="background-image: url('<?php echo get_template_directory_uri(); ?>/assets/images/landing/hero-bg3.png')">
     <img class="hero__image" src="<?php echo get_template_directory_uri(); ?>/assets/images/landing/people.png" alt="">
     <div class="hero__triangle-right"></div>
     <div class="hero__triangle-left"></div>
     <div class="hero__overlay"></div>
+
+    <div class="hero__text-wrapper hero__tomasz-wrapper">
+        <h1>Tomasz Bylica</h1>
+        <p>Radca prawny</p>
+        <a href="#">+48 123 456 789</a>
+    </div>
+
+    <div class="hero__text-wrapper hero__anna-wrapper">
+        <h1>Anna Bylica</h1>
+        <p>Radca prawny</p>
+        <a href="#">+48 123 456 789</a>
+    </div>
+
+    <!-- <div class="hero__bottom"></div> -->
 </div>
